25cba47b8de95e233efeb0a82bc9c3e8fbae4fce
g00400498 <G00400498@atu.ie>
2022-12-12 10:04:32 +0000

    new feature

--- main/java/ie/atu/exam/ShiftWorker.java
+++ main/java/ie/atu/exam/ShiftWorker.java
@@ -5,6 +5,9 @@ public class ShiftWorker {
     private String gender;
     private int shift;
     private int age;
+    boolean female;
+    boolean male;
+    boolean nonbinary;
 
     public ShiftWorker() {
     }
@@ -40,7 +43,92 @@ public class ShiftWorker {
     public String getAge() {
         return age;
     }
-    public void setAge(String age) {
+    public void setAge(int age) {
         this.age = age;
+    }
+
+    public ShiftWorker(String name){
+        if( name < 6){
+            System.out.println("Invalid Input. Minimum of 6 characters");
+        }
+        else if( name > 21){
+            System.out.println("Invalid Input. Maximum of 6 characters");
+        }
+        else{
+            System.out.println(name);
+        }
+    }
+
+    public ShiftWorker(String gender){
+        boolean f = female;
+        boolean m = male;
+        boolean n = nonbinary;
+
+        if (gender = f)
+        {
+            System.out.print("Female");
+        }
+
+        else if (gender = m)
+        {
+            System.out.print("Male");
+        }
+
+        else if (gender = n)
+        {
+            System.out.print("Non-Binary");
+        }
+
+        else{
+            System.out.print("Invalid Gender Type");
+        }
+    }
+
+    public void setGender(boolean gender)
+    {
+        if (gender = f)
+        {
+            System.out.print("Female");
+        }
+
+        else if (gender = m)
+        {
+            System.out.print("Male");
+        }
+
+        else if (gender = n)
+        {
+            System.out.print("Non-Binary");
+        }
+
+        else{
+            System.out.print("Invalid Gender Type");
+        }
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public ShiftWorker(int age){
+        if( age < 18){
+            System.out.println("Age must be at least 18");
+        }
+        else{
+            System.out.println(age);
+        }
+    }
+
+    public ShiftWorker(int shift){
+        System.out.println("Select 1 for Day shift or select 2 for  Night Shift");
+        if (shift = 1){
+            System.out.print("Day shift");
+        }
+        if else(shift = 2){
+            System.out.print("Night shift");
+        }
+        else{
+            System.out.println("Invalid Input")
+        }
     }
 }
